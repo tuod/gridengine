393ab61fe186f0e9b8f5a9bc3913dc9f2abbd37d
Stanislav Grebenyukov <stanislav@grebenyukov.ru>
2024-05-22 12:11:41 +0300

    Draw one hexagon

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,6 @@ import { v4 as uuidv4 } from "uuid";
 // Угол 60 градусов в радианах (любой угол гексагона)
 const angle60inRadian = (2 * Math.PI) / 6;
 
-function roundedArray(arr: number[], digits: number) {
-  let resultArray: number[] = [];
-  arr.forEach((element) => {
-    resultArray.push(parseFloat(element.toFixed(digits)));
-  });
-  return resultArray;
-}
-
 function roundUp(num: number, precision: number) {
   precision = Math.pow(10, precision);
   return Math.ceil(num * precision) / precision;
@@ -26,6 +18,15 @@ class Point {
   }
 }
 
+class Offset {
+  x: number;
+  y: number;
+  constructor(x = 0, y = 0) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
 class Line {
   first: Point;
   second: Point;
@@ -33,11 +34,12 @@ class Line {
     this.first = first;
     this.second = second;
   }
-  draw(ctx: CanvasRenderingContext2D, passMove: boolean = true) {
+  draw(ctx: CanvasRenderingContext2D, passMove: boolean = true): this {
     if (!passMove) {
       ctx.moveTo(Math.round(this.first.x), Math.round(this.first.y));
     }
     ctx.lineTo(Math.round(this.second.x), Math.round(this.second.y));
+    return this;
   }
 }
 
@@ -69,11 +71,80 @@ class Hexagon {
     }
     ctx.closePath();
     ctx.stroke();
-    return this
+    return this;
+  }
+
+  drawText(text: string, ctx: CanvasRenderingContext2D, offset = new Offset()) {
+    ctx.globalAlpha = 0.8;
+    ctx.textAlign = "center";
+    ctx.textBaseline = "middle";
+    ctx.font = this.radius * 0.4 + "px serif";
+    ctx.fillStyle = "black";
+    ctx.fillText(
+      text,
+      Math.round(this.center.x + offset.x),
+      Math.round(this.center.y + offset.y)
+    );
+    return this;
+  }
+}
+
+class HexGrid {
+  hexRadius: number;
+  canvas: HTMLCanvasElement;
+  ctx: CanvasRenderingContext2D;
+  constructor(hexRadius: number, canvas: HTMLCanvasElement) {
+    this.hexRadius = hexRadius;
+    this.canvas = canvas;
+    this.ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+    Object.entries({
+      load: window,
+      resize: window,
+      mousewheel: document,
+    }).forEach(([eventName, sourceObject]) => {
+      sourceObject.addEventListener(
+        eventName,
+        (event): void => {
+          this.handleEvent(event);
+        },
+        false
+      );
+    });
+  }
+
+  handleEvent(event: Event): void {
+    switch (event.type) {
+      case "mousewheel":
+        let wheelEvent = event as WheelEvent;
+        if (wheelEvent.deltaY >= 0) {
+          if (this.hexRadius < 100) {
+            this.hexRadius += 1;
+          }
+        } else {
+          if (this.hexRadius > 25) {
+            this.hexRadius -= 1;
+          }
+        }
+      default:
+        this.draw();
+    }
+  }
+
+  fitCanvasToWindow() {
+    this.canvas.width = window.innerWidth - 25;
+    this.canvas.height = window.innerHeight - 25;
+  }
+
+  draw() {
+    this.fitCanvasToWindow();
+    new Hexagon(new Point(this.hexRadius, this.hexRadius), this.hexRadius)
+      .drawBorder(this.ctx)
+      .drawText("top", this.ctx, new Offset(0, this.hexRadius * -0.35))
+      .drawText("middle", this.ctx)
+      .drawText("bottom", this.ctx, new Offset(0, this.hexRadius * 0.35));
   }
 }
 
 const gridCanvas = document.getElementById("gridCanvas") as HTMLCanvasElement;
 // const overlayCanvas = document.getElementById("overlayCanvas");
-let ctx = (gridCanvas.getContext("2d") as CanvasRenderingContext2D);
-new Hexagon(new Point(50, 50), 50).drawBorder(ctx)
+new HexGrid(50, gridCanvas);
